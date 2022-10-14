a7be30c1121e227354c4b9c433205b4b0acb00c6
trupti <truptiauti149@gmail.com>
2022-10-14 23:30:11 +0530

    ArrayList program is added

---
+++ Arraydemo/src/com/demo/ArrayList.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+		List<String> list =Arrays.asList("Trupti","rushi");
+		System.out.println(list);
+
+	}
+
+}
